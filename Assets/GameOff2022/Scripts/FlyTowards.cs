bd50c0706f8c6364cacf9dd11921ea370c927406
erich.smith <erich.smith@atcss.com>
2022-11-13 10:04:03 -0500

    Update FlyTowards.cs
    Testing AI behaviour.

--- Assets/GameOff2022/Scripts/FlyTowards.cs
+++ Assets/GameOff2022/Scripts/FlyTowards.cs
@@ -1,55 +1,144 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class FlyTowards : MonoBehaviour
+namespace LighterThanAir
 {
-    [SerializeField]
-    private Transform targetTransform;
-
-    [SerializeField]
-    private float flyingSpeed = 10.0f;
-    [SerializeField] 
-    private float turningSpeed = 2.0f;
-    [SerializeField]
-    private float distanceToFlyPast = 20.0f;
-    [SerializeField]
-    private float maxRollRotation = 30.0f;
-    private Vector3 targetPosition;
-    
-    private float distanceToTarget;
-
-    // Start is called before the first frame update
-    void Start()
+    public class FlyTowards : MonoBehaviour
     {
-        // Cache transform copy.
-        this.targetPosition = this.targetTransform.position;
-        this.targetPosition.y = this.transform.position.y;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // Determine which direction to rotate towards
-        Vector3 targetDirection = targetPosition - transform.position;
-        distanceToTarget = targetDirection.magnitude;
-
-        // The step size is equal to speed times frame time.
-        float singleStep = turningSpeed * Time.deltaTime;
-
-        // If close to target, fly straight past then turn around for another pass.
-        if (distanceToTarget > distanceToFlyPast)
+        private enum State
         {
+            LockedOnTarget,
+            FlyingPastTarget,
+            AcquiringTarget
+        };
+
+        private enum TurnDirection
+        {
+            Left = -1,
+            Right = 1
+        };
+
+        [SerializeField] private Transform targetTransform;
+        [SerializeField] private float flyingSpeed = 10.0f;
+        [SerializeField]  private float yawRate = 2.0f;
+        [SerializeField]  private float rollRate = 5.0f;
+        [SerializeField] private float distanceToFlyPast = 20.0f;
+        [SerializeField] private float maxRollAngle = 30.0f;
+        [SerializeField] private float tooCloseToAttack = 10.0f;
+
+        private Vector3 yOffset;
+        private State currentState = State.AcquiringTarget;
+        private TurnDirection nextTurnDirection = TurnDirection.Right;
+
+        void Start()
+        {
+            yOffset = Vector3.up * transform.position.y;
+        }
+
+        void Update()
+        {
+            SetNextTurnDirection();
+
+            if (TooCloseToAttack())
+            {
+                SwitchState(State.FlyingPastTarget);
+            }
+            
+            switch (currentState)
+            {
+                case State.AcquiringTarget:
+                    AcquireTarget();
+                    break;
+
+                case State.LockedOnTarget:
+                    FlyAtTarget();
+                    break;
+
+                case State.FlyingPastTarget:
+                    FlyPastTarget();
+                    break;
+
+                default:
+                    break;
+            }
+
+            // Fly forward.
+            this.transform.Translate(Vector3.forward * flyingSpeed * Time.deltaTime);
+
+            DrawHelpers();
+        }
+
+        private void AcquireTarget()
+        {
+            // Determine which direction to rotate towards
+            Vector3 targetDirection = (targetTransform.position - transform.position) + yOffset;
+
+            // Get delta angle between current forward vector and the vector towards target.
+            float angleDiff = Vector3.Angle(transform.forward, targetDirection);
+            if (angleDiff < Mathf.Epsilon)
+            {
+                SwitchState(State.LockedOnTarget);
+                return;
+            }
+
+            // The step size is equal to speed times frame time.
+            float yawStep = yawRate * Time.deltaTime;
+
             // Rotate the forward vector towards the target direction by one step
-            Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+            Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, yawStep, 0.0f);
 
-            // Draw a ray pointing at our target in
-            //Debug.DrawRay(transform.position, newDirection, Color.red);
-
-            // Calculate a rotation a step closer to the target and applies rotation to this object
+            // Calculate a rotation a step closer to the target and applies rotation to this object.
             transform.rotation = Quaternion.LookRotation(newDirection);
         }
-        
-        this.transform.Translate(Vector3.forward * flyingSpeed * Time.deltaTime);
+
+        private void FlyAtTarget()
+        {
+            
+        }
+
+        private void FlyPastTarget()
+        {
+            float distanceToTarget = ((targetTransform.position - transform.position) + yOffset).magnitude;
+            if (distanceToTarget > distanceToFlyPast)
+            {
+                SwitchState(State.AcquiringTarget);
+            }
+        }
+
+        private void DrawHelpers()
+        {
+            Debug.DrawLine(transform.position, targetTransform.position, Color.magenta, Time.deltaTime);
+            Debug.DrawLine(transform.position, transform.position + transform.forward * 10, Color.blue, Time.deltaTime);
+            Debug.DrawLine(transform.position, transform.position + transform.up * 10, Color.green, Time.deltaTime);
+            Debug.DrawLine(transform.position, transform.position + transform.right * 10, Color.red, Time.deltaTime);
+        }
+
+        private void SwitchState(State newState)
+        {
+            currentState = newState;
+        }
+
+        private bool TooCloseToAttack()
+        {
+            float distanceToTarget = ((targetTransform.position - transform.position) + yOffset).magnitude;
+
+            return distanceToTarget < tooCloseToAttack ? true : false;
+        }
+
+        private float NormalizeValue(float value, float min, float max)
+        {
+            return (value - min) / (max - min);
+        }
+
+        private void SetNextTurnDirection()
+        {
+            if (transform.rotation.eulerAngles.y >= 180.0f)
+            {
+                nextTurnDirection = TurnDirection.Right;
+            }
+            else
+            {
+                nextTurnDirection = TurnDirection.Left;
+            }
+        }
     }
 }
