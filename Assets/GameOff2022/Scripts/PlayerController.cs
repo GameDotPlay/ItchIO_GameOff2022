00586c3ff8e69332bf44596c92ddb5f9b7c85f44
erich.smith <gamedotplay0@gmail.com>
2022-11-17 16:56:04 -0500

    Added PlayerController script.

---
+++ Assets/GameOff2022/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace LighterThanAir
+{
+    public class PlayerController : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
