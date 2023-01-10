39612336c7bd3b784bfd374d5fd0a4d8d2353fcb
Erich Smith <gamedotplay0@gmail.com>
2023-01-10 11:05:53 -0500

    Target tracking updates.

--- Assets/GameOff2022/Scripts/RotatingGunMount.cs
+++ Assets/GameOff2022/Scripts/RotatingGunMount.cs
@@ -4,44 +4,75 @@ namespace LighterThanAir
 {
     public class RotatingGunMount : MonoBehaviour
     {
-        [SerializeField] private float fireAngleRange = 150.0f;
+        #region Property Panel
+        [SerializeField] private GameObject target;
+		[SerializeField] private float fireAngleRange = 80.0f;
+		[SerializeField] private float YawRate = 1.0f;
+        [SerializeField] private bool clampRotation = true;
+        #endregion
 
-        public GameObject Target { get; set; }
-        public float YawRate { get; set; }
+        #region Public 
+        public float TargetDotProduct { get; private set; }
+		public float DotProductLimit { get; private set; }
+		#endregion
 
-        private Vector3 initialRotation;
+		#region Private
+		private GunBarrels child = null;
+		private Vector3 initialRotation;
+        private Vector3 initialForward;
+        
+		#endregion
 
-        private void Start()
+		private void Awake()
+		{
+			this.child = (GunBarrels)this.GetComponentInChildren(typeof(GunBarrels));
+		}
+
+		private void Start()
         {
-            initialRotation = this.transform.rotation.eulerAngles;
+			this.initialRotation = this.transform.rotation.eulerAngles;
+            this.initialForward = this.transform.forward;
+            Transform maxAngleTransform = this.transform;
+            maxAngleTransform.rotation = Quaternion.Euler(this.initialRotation.x, this.initialRotation.y + this.fireAngleRange, this.initialRotation.z);
+			this.DotProductLimit = Vector3.Dot(this.initialForward, maxAngleTransform.forward);
         }
 
         private void LateUpdate()
         {
-            if (Target == null)
+            if (this.target == null)
             {
                 return;
             }
 
-            Vector3 direction = ((Target.transform.position - transform.position) - (Vector3.up * Target.transform.position.y)).normalized;
+            Vector3 lastRotation = this.transform.rotation.eulerAngles;
+            Vector3 direction = (this.target.transform.position - this.transform.position).normalized;
 
             Quaternion goalRotation = Quaternion.LookRotation(direction);
 
-            transform.rotation = Quaternion.Slerp(transform.rotation, goalRotation, YawRate);
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, goalRotation, YawRate);
+            this.transform.rotation = Quaternion.Euler(this.initialRotation.x, this.transform.rotation.eulerAngles.y, this.initialRotation.z);
 
-            ClampRotationAngle();
+            this.TargetDotProduct = Vector3.Dot(this.initialForward, direction);
+            Debug.Log(TargetDotProduct);
+
+			if (this.clampRotation)
+            {
+				this.ClampRotationAngle(lastRotation);
+			}
         }
 
-        private void ClampRotationAngle()
+		private void ClampRotationAngle(Vector3 lastRotation)
         {
-            if (transform.rotation.eulerAngles.y - initialRotation.y > fireAngleRange / 2)
+            if (this.TargetDotProduct < this.DotProductLimit)
             {
-                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, initialRotation.y + (fireAngleRange / 2), transform.rotation.eulerAngles.z);
+                this.transform.rotation = Quaternion.Euler(this.transform.rotation.eulerAngles.x, lastRotation.y, this.transform.rotation.eulerAngles.z);
             }
-            else if (initialRotation.y + transform.rotation.eulerAngles.x < fireAngleRange)
-            {
-                transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, initialRotation.y - (fireAngleRange / 2), transform.rotation.eulerAngles.z);
-            }
+        }
+
+        public void SetTarget(GameObject target)
+        {
+            this.target = target;
+            this.child.SetTarget(target);
         }
     }
 }
